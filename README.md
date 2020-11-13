fabd086e45e75f1510f96258d0727a99f65ee935
Montvydas Klumbys <montvydas.klumbys@gmail.com>
2020-11-13 18:14:47 +0200

    Added the tool, updated the readme file, add example subtitle file and also added example image.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# translatesubs
+# TranslateSubs
 It's a tool to translate subtitles into any language, that is supported by google translator :)
+
+# About
+
+The translator can be either used to extract the subtitle from the video file and then perform translation on that file or process a subtitle file instead. The first required an ffpmeg installed and setup to work from terminal. If you extract subtitle yourself, note that often the file format for subtitles is SRT. This only has minimal styling and anime usually ASS format, which can even do animations. I recommend sticking to ACC if you want the styling to remain.
+
+# Usage
+
+To translate an existing subtitle file (e.g. the provided truncated.ass) and translate to Spanish (default is Spanish):
+`python substranslator.py truncated.ass out.ass --to_lang es`
+This will generate out.ass subtitle file, which can be imported in the VLC player via `Subtitles -> Add Subtitle File...`
+
+If a video file is being used instead e.g. video.mkv, add --video_file flag and the first parameter becomes a video file:
+`python substranslator.py video.mkv out.ass --video_file`
+
+If you would like to learn a new language you might as well show both the original AND the translated languages (original letters are smaller and slightly transparent, as shown in the example picture) using flag --combine:
+`python substranslator.py truncated.ass out.ass --combine`
+
+Some video files might have multiple subtitle tracks. You can select the track you want to use (starting from 0) using argument --subs_track:
+`python substranslator.py video.mkv out.ass --video_file --subs_track 1`
+
+# Note
+
+The tool uses a free googletrans API, which uses one of the google domains e.g. translate.google.com or translate.google.co.uk to perform translation. After a couple of calls that domain gets blocked and thus another one is selected instead. I added 17 domains, which should ensure that you will always have a domain that still works, because after about 1h that domain gets unblocked. Don't worry, you can still go to chrome and use the google translate :)
---
+++ translatesubs.py
@@ -0,0 +1,107 @@
+import pysubs2
+import argparse
+import json
+import configparser
+from googletrans import Translator
+import subprocess
+
+
+def translate_sub_file(file_name, to_lang, combine):
+    subs = pysubs2.load(file_name)
+    translated_subs = translate_subs(subs, to_lang)
+
+    print('total subs:', len(subs))
+    print('total translated:', len(translated_subs))
+
+    # Update the original subs with the translation and also combine them if needed
+    return update_subs(subs, translated_subs, combine)
+
+
+def translate_subs(subs, to_lang):
+    # Separator was chosen after noting that not all will be treated as non-text object and others
+    # such as @@ will not translate all words e.g. Connect  @@  Something else... Will not translate Connect :/
+    # While Connect  ##  Something else... will do.
+    separator = ' ## '
+    # logic is that it get's rid of line based styling if has one. Noticed, that sometimes plaintext is None, then
+    # simply use the text. It would be better if anything within brackets {...} would be preserved, but it's ok for now.
+    plaintext_subs = [to_plaintext(sub) for sub in subs]
+    # Combine by some GOOD separator, such as ' ## ' that google translate would keep in place instead of removing
+    # after translation. This will allow us to send all of the text to be sent for translation, yet still track of the
+    # what lines belong to which timestamp
+    combined_subs_to_translate = separator.join(plaintext_subs)
+    # Send ALL of the text file for translation. It might be too large, then gotta crop it somehow,
+    # but didn't experience this problem yet so for now good enough!
+    combined_subs_translated = translate_text(combined_subs_to_translate, to_lang)
+    # Separate the subs by the used separator
+    return combined_subs_translated.split(separator)
+
+
+def update_subs(original_subs, translated_subs, combine):
+    for i, sub in enumerate(original_subs):
+        # For now ignore the line based styling.
+        # Also replace \n with \N as otherwise only the first subtitle line will be shown as others will be treated as
+        # separate event Could just write into plaintext, but if combined flag is used, then still have to perform these
+        original = replace_with_capital_newline(to_plaintext(sub))
+        translated = replace_with_capital_newline(translated_subs[i])
+
+        original_styled = style_down(original) if combine else ''
+        sub.text = f'{translated}{original_styled}'
+    return original_subs
+
+
+def style_down(text):
+    # Make text smaller than original as it's less important, plus add transparency
+    style = '\\N{\\fscx70\\fscy70\\alpha&H80&}'
+    return f'{style}{text}'
+
+
+def translate_text(text, to_lang):
+    # Google API provider should allow new access every 1h, but if more translations need to be done,
+    # a number of different country providers are given
+    provider_endings = ['com', 'co.kr', 'lt', 'ru', 'es', 'lv', 'ee', 'pl', 'de', 
+                        'sk', 'fr', 'co.uk', 'ae', 'ro', 'gy', 'pt', 'ms']
+    provider_base = 'translate.google.'
+    
+    for ending in provider_endings:
+        provider = f'{provider_base}{ending}'
+        translator = Translator(service_urls=[provider])
+        try:
+            return translator.translate(text, dest=to_lang).text
+        except AttributeError:
+            print(f'Provider "{provider}" got blocked :/')
+    print('No more providers left to try, try updating the provider list.')
+    exit()
+
+
+def replace_with_capital_newline(multiline):
+    return multiline.replace('\n', '\\N')
+
+
+def to_plaintext(sub):
+    return sub.plaintext if sub.plaintext else sub.text
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='python translatesubs.py input.srt output.srt es')
+    parser.add_argument('input', type=str,
+                        help='input subtitle file by default. If flag --video_file set, then this is video file name.')
+    parser.add_argument('output', type=str, help='output subtitles file')
+    parser.add_argument('--to_lang', default='es', type=str, help='language to which translate to')
+    parser.add_argument('--combine', action='store_true', help='Set this if you want to combine two languages at once.')
+
+    parser.add_argument('--video_file', action='store_true',
+                        help='Set this if video file is used instead of subtitle file.')
+    parser.add_argument('--subs_track', default=0, type=int,
+                        help='Subtitle tract, if video has multiple tracks. Default is 0.')
+    args = parser.parse_args()
+
+    subs_file = args.input
+
+    # If we process the video rather than subtitle file, then simply extract the subtitle and place it into output
+    if args.video_file:
+        operation = ['ffmpeg', '-i', args.input, '-map', f'0:s:{args.subs_track}', args.output]
+        subprocess.run(operation)
+        subs_file = args.output
+
+    # now pick the correct subtitle file and perform translation
+    translate_sub_file(subs_file, args.to_lang, args.combine).save(args.output)
